43316765f1c7f63abb66e05f67e658c30d1b293a
choisinsuk <990109aa@gmail.com>
2024-09-13 16:02:09 +0900

    back

---
+++ src/main/java/com/hub/BuffetHubApplication.java
@@ -0,0 +1,13 @@
+package com.hub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuffetHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuffetHubApplication.class, args);
+	}
+
+}
