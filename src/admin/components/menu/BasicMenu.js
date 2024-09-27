a23a58263807352c1a8c117bb66fee5b1b0869a0
loggodoggo <tjrals809@naver.com>
2024-09-27 14:57:03 +0900

    layout

--- src/admin/components/menu/BasicMenu.js
+++ src/admin/components/menu/BasicMenu.js
@@ -3,34 +3,38 @@ import { Link } from "react-router-dom";
 
 const BasicMenu = () => {
   return (
-    <nav id="navbar" className=" flex bg-blue-300">
-        <div className=" bg-orange-200 p-4 font-medium">
+    <nav id="navbar" className=" flex">
+        <div className=" bg-orange-200 p-1 font-medium">
           <img src={logo} alt="Logo" />
         </div> 
 
-      <div className="w-4/5 bg-orange-300 flex justify-center">
-        <ul className="flex p-4 text-white font-bold justify-center ">
+      <div className="w-full bg-orange-400">
+        <ul className="flex p-4 text-white font-bold">
 
-          <li className="pr-6 text-2xl text-center">
-            <Link to={"/"}>메인</Link>
+          <li className="pr-6 text-2xl">
+            <Link to={"/"}>메인 페이지</Link>
           </li>
 
-          <li className="pr-6 text-2xl text-center">
-            <Link to={"/reserve"}>예약 관리</Link>
+          <li className="pr-6 text-2xl">
+            <Link to={"/about"}>예약 관리</Link>
           </li>
 
-          <li className="pr-6 text-2xl text-center">
-            <Link to={"/infoManager"}>뷔페 정보 관리</Link>
+          <li className="pr-6 text-2xl">
+            <Link to={"/about"}>뷔페 예약 설정</Link>
           </li>
 
-          <li className="pr-6 text-2xl text-center">
-            <Link to={"/userManager"}>회원관리</Link>
+          <li className="pr-6 text-2xl">
+            <Link to={"/Notice"}>뷔페 정보 설정</Link>         
           </li>
+
+          <li className="pr-6 text-2xl">
+            <Link to={"/CustomErinquiry"}>게시판 관리</Link>
+          </li>
+
+          <li className="pr-6 text-2xl">
+            <Link to={"/ReviewBoard"}>회원 관리</Link>
+          </li>  
         </ul>
-
-      </div>
-      <div className="w-1/5 flex justify-end bg-orange-200 p-4 font-medium">
-        <div className="text-black font-bold text-3xl"><Link to={"/reserve"}>로그인</Link></div>
       </div>
     </nav>
   );
