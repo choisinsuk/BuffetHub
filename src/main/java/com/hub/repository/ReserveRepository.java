4becafc611d8ef0a754f09817cbb8147e537be31
leejaejun <roy2619@naver.com>
2024-10-14 15:40:17 +0900

    예약부분 완성

--- src/main/java/com/hub/repository/ReserveRepository.java
+++ src/main/java/com/hub/repository/ReserveRepository.java
@@ -1,15 +1,15 @@
 package com.hub.repository;
 
-import java.util.List;
-
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.hub.domain.Reserve;
-import com.hub.dto.ReserveListDTO;
+import com.hub.domain.User;
 
 public interface ReserveRepository extends JpaRepository<Reserve, Long>{
 	
-//	@Query("select tbl")
-//	public List<ReserveListDTO> get
+	// urId로 데이터 조회하는 메서드 추가
+    Page<Reserve> findByUser(User user, Pageable pageable);
 
 }
