7a67026c4e87667e236446cf7dc24410ba0532d8
yong <htiaruau@naver.com>
2024-10-18 15:15:51 +0900

    마커 추가

--- buffethubfrontend/src/component/buffetinfo/KakaoMapComponent.js
+++ buffethubfrontend/src/component/buffetinfo/KakaoMapComponent.js
@@ -11,6 +11,18 @@ function KakaoMapCompnent() {
     };
 
     const map = new kakao.maps.Map(container, options); // 지도 생성 및 객체 리턴
+
+    let markerPosition = new kakao.maps.LatLng(
+      37.476761, 126.879946
+    );
+
+    // 마커 생성
+    let marker = new kakao.maps.Marker({
+      position: markerPosition,
+    });
+
+    marker.setMap(map);
+
   }, []);
 
   return (
