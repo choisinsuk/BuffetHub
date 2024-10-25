8c09a32970a0dc89820a3e66d924290035cf12d1
yong <htiaruau@naver.com>
2024-10-25 14:33:10 +0900

    // 회원가입 엔드포인트 및 관리자 페이지 접근 권한 설정 주석처리하기...

--- src/main/java/com/hub/config/CustomSecurityConfig.java
+++ src/main/java/com/hub/config/CustomSecurityConfig.java
@@ -49,7 +49,6 @@ public class CustomSecurityConfig {
 		http.sessionManagement(sessionConfig -> sessionConfig.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 		http.csrf(config -> config.disable());
 
-		
 		http.formLogin(config -> {
 			config.loginPage("/api/user/login");
 			config.successHandler(new APILoginSuccessHandler()); // 로그인 후 처리는 APILoginSuccessHandler
@@ -62,16 +61,15 @@ public class CustomSecurityConfig {
 			config.accessDeniedHandler(new CustomAccessDeniedHandler());
 		});
 
-		// 회원가입 엔드포인트 및 관리자 페이지 접근 권한 설정
-		http.authorizeHttpRequests(authorizeRequests -> {
-			authorizeRequests
-					.requestMatchers("/api/user/register", "/api/user/login", "api/user/checkId/{urId}").permitAll() // 인증 없이 회원가입 엔드포인트 접근 허용
-					.requestMatchers("/api/user/**").hasRole("USER")// 사용자 페이지 접근 권한 설정
-					.requestMatchers("/api/reserve/**").hasRole("USER")// 사용자 페이지 접근 권한 설정
-					.requestMatchers("/api/admin/**").hasRole("ADMIN")// 관리자 페이지 접근 권한 설정
-					.anyRequest().authenticated(); // 그 외의 모든 요청은 인증 필요
-		});
-
+		/*
+		 * // 회원가입 엔드포인트 및 관리자 페이지 접근 권한 설정 http.authorizeHttpRequests(authorizeRequests
+		 * -> { authorizeRequests.requestMatchers("/api/user/register",
+		 * "/api/user/login", "/api/user/checkId/{urId}") .permitAll() // 인증 없이 회원가입
+		 * 엔드포인트 접근 허용 .requestMatchers("/api/user/**").hasRole("USER")// 사용자 페이지 접근 권한
+		 * 설정 .requestMatchers("/api/reserve/**").hasRole("USER")// 사용자 페이지 접근 권한 설정
+		 * .requestMatchers("/api/admin/**").hasRole("ADMIN")// 관리자 페이지 접근 권한 설정
+		 * .anyRequest().authenticated(); // 그 외의 모든 요청은 인증 필요 });
+		 */
 
 		return http.build();
 	}
