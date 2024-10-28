9e9eb8e8c0c05094fac2dc09c02ef4b14087cc96
sch0739 <sch0739@gmail.com>
2024-10-28 20:21:54 +0900

    고객문의 DTO 입니다.

---
+++ src/main/java/com/hub/dto/CustomerInquiryBoardDTO.java
@@ -0,0 +1,28 @@
+package com.hub.dto;
+
+import java.time.LocalDateTime;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.Builder;
+
+// 고객 문의 DTO 클래스
+@Getter
+@Setter
+@Builder
+public class CustomerInquiryBoardDTO {
+
+    private Long cqNb; // 고객 문의 번호
+
+    private String usId; // 회원 아이디
+
+    private String cqTitle; // 고객 문의 제목
+
+    private String cqCtt; // 고객 문의 내용
+
+    private LocalDateTime cqRegdt; // 고객 문의 등록 일자
+
+    private String cqAnswerCtt; // 고객 문의 답변 내용
+
+    private LocalDateTime cqAnswerRegdt; // 고객 문의 답변 등록 일자
+
+}
